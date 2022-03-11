68ea7ed7b461a762704550bb3e8d6a05522ac0ef
ShaileshKV29 <shailesh29062001@gmail.com>
2022-03-11 12:56:51 +0530

    test code

---
+++ test.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int A[3] = {0,0,0};
+    for(int i = 0; i < 4; i++)
+    {
+        cout << A[i] << " " << i << endl;
+
+    }
+
+    cout << endl;
+    // cout << A[0] << endl;
+    // cout << A[1] << endl;
+    // cout << A[2] << endl;
+    // cout << A[3] << endl;
+    // cout << A[3] << endl;
+    cout << A[3] << endl;
+
+    return 0;
+}
